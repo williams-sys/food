c8d7f29309dbf22e6c6b48cffec4e8c262566e87
williamss07-shann-htet <shanhtet07@gmail.com>
2020-11-18 20:59:39 +0630

    Added a new file

---
+++ index.js
@@ -0,0 +1,120 @@
+let screenHeight = $(window).height();
+// console.log(screenHeight);
+
+
+$(window).scroll(function () {
+    let currentPosition = $(this).scrollTop();
+    // console.log(currentPosition);
+    if(currentPosition > screenHeight-100){
+        $(".site-nav").addClass("site-nav-scroll");
+    }else{
+        $(".site-nav").removeClass("site-nav-scroll");
+        setActive("home");
+    }
+});
+
+$(".navbar-toggler").click(function () {
+    let result = $(".navbar-collapse").hasClass("show");
+    console.log(result);
+
+    if(result){
+
+        $(".menu-icon").removeClass("fa-close").addClass("fa-bars");
+
+
+
+    }else{
+
+        $(".menu-icon").removeClass("fa-bars").addClass("fa-close");
+
+
+    }
+
+});
+
+function setActive(current) {
+
+    $(".nav-link").removeClass("current-section");
+
+    $(`.nav-link[href='#${current}']`).addClass('current-section');
+
+}
+
+function navScroll() {
+
+    let currentSection = $("section[id]");
+    currentSection.waypoint(function (direction) {
+
+        if(direction == "down"){
+            let currentSectionId = $(this.element).attr('id');
+            console.log(currentSectionId);
+            setActive(currentSectionId);
+        }
+
+    },{ offset:'150px' });
+
+    currentSection.waypoint(function (direction) {
+
+        if(direction == "up"){
+            let currentSectionId = $(this.element).attr('id');
+            console.log(currentSectionId);
+            setActive(currentSectionId);
+        }
+
+    },{ offset:'150px' });
+
+}
+navScroll();
+
+//for menu bar up
+$(`.nav-link`).click(function(){
+    $(`.navbar-collapse`).removeClass(`show`);
+    $(`.menu-icon`).addClass(`fa-bars`);
+});
+
+$(window).on("load",function () {
+    $('.loader-container').fadeOut(500,function () {
+        $(this).remove();
+    });
+});
+
+
+
+
+
+$(".slide").slick({
+    arrows:true,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1400,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
